53be870df63a48abc2487fce17fecf7c25518903
Andres <ballerlikemike23@gmail.com>
2018-07-02 19:23:38 -0400

    Working with props

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <Person name='Andres' age='29' />
+        <Person name='Max' age='27'>My Hobbies: Racing</Person>
+        <Person name='Steph' age='25' />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?')) // What JSX boils down too.. simply just JS
